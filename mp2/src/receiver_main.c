634809fc2df4e34321ca7fb415ad0d2b88310ff1
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-10-26 03:39:40 -0500

    ++

--- mp2/src/receiver_main.c
+++ mp2/src/receiver_main.c
@@ -16,55 +16,112 @@
 #include <unistd.h>
 #include <pthread.h>
 
+typedef unsigned long long int ull;
+typedef unsigned short int us;
+#define payload 1400
+const int receiveBuffer = 2000;
+
+typedef struct {
+	ull seqNum;
+	ull length;
+	char end;
+	char data[payload+1];
+} segment;
+
+/* Parameters */
+ull nextByteExpected = 1;
 
 
-struct sockaddr_in si_me, si_other;
-int s, slen;
+void diep(char *s);
+void writeFile(char* destinationFile, segment** buffer, ull packetNum);
+
+void reliablyReceive(us myUDPport, char* destinationFile) {
+	/* Open socket and bind */
+	struct sockaddr_in si_me, si_other;
+	int s;
+	socklen_t slen;
+    slen = sizeof (si_other);
+    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+        diep("socket");
+    memset((char *) &si_me, 0, sizeof (si_me));
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(myUDPport);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+    printf("Now binding\n");
+    if (bind(s, (struct sockaddr*)&si_me, sizeof (si_me)) == -1)
+        diep("bind");
+
+	/* Now receive data and send acknowledgements */
+    segment* buffer[receiveBuffer] = {NULL};
+    ull packetNum = 0;
+	while (1) {
+    	segment packet;
+		recvfrom(s, &packet, sizeof(packet), 0,
+			(struct sockaddr *)&si_other, &slen);
+		ull seqNum = packet.seqNum;
+		if (seqNum == nextByteExpected)
+			nextByteExpected++;
+		if (!buffer[seqNum]) {
+			segment* cur = malloc(sizeof(segment));
+			cur->seqNum = packet.seqNum;
+			cur->length = packet.length;
+			cur->end = packet.end;
+			cur->data = packet.data;
+			buffer[seqNum] = cur;
+		}
+		if (packet.end == '1')
+			packetNum = seqNum;
+		if (packetNum && nextByteExpected > packetNum)
+			break;
+	}
+	// ull numbytes, total = 0;
+	// while (recvfrom(s, &packet, sizeof(packet), 0,
+	// (struct sockaddr *)&si_other, &slen)) {
+	// 	FILE* fp = fopen(destinationFile, "a+");
+	// 	numbytes = packet.length;
+	// 	packet.data[numbytes] = '\0';
+	// 	total += numbytes;
+	// 	fwrite(packet.data, 1, numbytes, fp);
+	// 	// printf("File written, cumulative %lld bytes.\n", total);
+	// 	fclose(fp);
+	// 	if (packet.end == '1')
+	// 		break;
+	// }
+	writeFile(destinationFile, buffer, packetNum);
+    close(s);
+	printf("%s received.\n", destinationFile);
+    return;
+}
 
 void diep(char *s) {
     perror(s);
     exit(1);
 }
 
-
-
-void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
-    
-    slen = sizeof (si_other);
-
-
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-        diep("socket");
-
-    memset((char *) &si_me, 0, sizeof (si_me));
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(myUDPport);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-    printf("Now binding\n");
-    if (bind(s, (struct sockaddr*) &si_me, sizeof (si_me)) == -1)
-        diep("bind");
-
-
-    /* Now receive data and send acknowledgements */    
-
-    close(s);
-    printf("%s received.", destinationFile);
-    return;
+void writeFile(char* destinationFile, segment** buffer, int packetNum) {
+	FILE* fp = fopen(destinationFile, "w");
+	ull total = 0;
+	for (int i = 1; i <= packetNum; i++) {
+		segment* packet = buffer[i];
+		ull length = packet->length;
+		total += length;
+		fwrite(packet->data, 1, length, fp);
+	}
+	fclose(fp);
+	printf("File written, total %lld bytes.\n", total);
 }
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
-    unsigned short int udpPort;
+    us udpPort;
 
     if (argc != 3) {
         fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
         exit(1);
     }
 
-    udpPort = (unsigned short int) atoi(argv[1]);
+    udpPort = (us) atoi(argv[1]);
 
     reliablyReceive(udpPort, argv[2]);
 }
+
--- mp2/src/sender_main.c
+++ mp2/src/sender_main.c
@@ -19,99 +19,230 @@
 #include <signal.h>
 #include <string.h>
 #include <sys/time.h>
-
+#include <math.h>
 #include <semaphore.h>
-#include <stdbool.h>
 
+typedef unsigned long long int ull;
+typedef unsigned short int us;
+#define payload 1400
+
+typedef struct {
+	ull seqNum;
+	ull length;
+	char end;
+	char data[payload+1];
+} segment;
+
+/* Socket parameters*/
 struct sockaddr_in si_other;
-int s, slen;
+int s;
+socklen_t slen;
 
-void diep(char *s) {
+/* Parameters shared by 2 threads */
+segment** packetBuffer;
+enum Congestion_Control{SS, CA, FR};
+int mode = SS;
+int dupACKcount = 0;
+int timeOutInterval = 50; // 50ms
+ull ssthresh = 1000;
+ull MSS = 10;
+ull cwnd = 10;
+ull sendBase = 1;
+ull nextSeqNum = 1;
+sem_t mutex;
+
+void diep(char* s);
+ull readSize(char* filename, ull bytesToTransfer);
+void storeFile(char* filename, int packetNum, ull actualBytes);
+void* threadRecvRetransmit();
+
+void reliablyTransfer(char* hostname, us hostUDPport, char* filename, ull bytesToTransfer) {
+	/* Open socket */
+    slen = sizeof(si_other);
+    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+        diep("socket");
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(hostUDPport);
+    if (inet_aton(hostname, &si_other.sin_addr) == 0)
+    	diep("inet_aton() failed\n");
+
+    /* Open file and store data into packet_buffer */
+    ull actualBytes = readSize(filename, bytesToTransfer);
+    int packetNum = ceil(actualBytes/(float)payload);
+    packetBuffer = malloc((packetNum+1) * sizeof(segment*));
+    storeFile(filename, packetNum, actualBytes);
+
+	/* Send data and receive acknowledgements on s */
+	for (int i = 1; i <= packetNum; i++) {
+		segment* packet = packetBuffer[i];
+		sendto(s, packet, sizeof(segment), 0,
+			(struct sockaddr *)&si_other, slen);
+	}
+	// pthread_t recvThread;
+	// sem_init(&mutex, 0, 1);
+	// while (1) {
+	// 	if (nextSeqNum > packetNum)
+	// 		break;
+	// 	sem_wait(&mutex);
+	// 	ull wnEnd = sendBase + cwnd;
+	// 	sem_post(&mutex);
+	// 	while (nextSeqNum <= packetNum && nextSeqNum < wnEnd) {
+	// 		segment* packet = packetBuffer[nextSeqNum];
+	// 		sendto(s, packet, sizeof(*packet), 0,
+	// 			(struct sockaddr *)&si_other, slen);
+	// 		if (nextSeqNum == 1)
+	// 			pthread_create(&recvThread, NULL, threadRecvRetransmit, NULL);
+	// 		nextSeqNum++;
+	// 	}
+	// }
+	// pthread_join(recvThread, NULL);
+    printf("%lld bytes sent.\n", actualBytes);
+    /* Release memory */
+	for (int i = 1; i <= packetNum; i++)
+		free(packetBuffer[i]);
+	free(packetBuffer);
+	sem_destroy(&mutex);
+    printf("Closing the socket\n");
+    close(s);
+    return;
+}
+
+void* threadRecvRetransmit() {
+	struct timeval timeout;      
+    timeout.tv_sec = 0;
+    timeout.tv_usec = timeOutInterval * 1000;
+    setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    while (1) {
+    	int ack;
+    	int numbytes = recvfrom(s, &ack, sizeof(ack), 0,
+    		(struct sockaddr *)&si_other, &slen);
+    	if (numbytes == -1) {
+    		/* Timeout */
+    		segment* packet = packetBuffer[sendBase];
+			sendto(s, packet, sizeof(segment), 0,
+				(struct sockaddr *)&si_other, slen);
+			mode = SS;
+			ssthresh = cwnd / 2;
+			sem_wait(&mutex);
+			cwnd = MSS;
+			sem_post(&mutex);
+			dupACKcount = 0;
+			continue;
+    	}
+    	if (ack == 0)
+    		break;
+    	if (mode == SS) { // Slow start
+    		if (ack == sendBase) {
+    			dupACKcount++;
+    			if (dupACKcount == 3) {
+    				mode = FR;
+					ssthresh = cwnd / 2;
+    				sem_wait(&mutex);
+					cwnd = MSS;
+					sem_post(&mutex);
+    			}
+    		} else if (ack > sendBase) {
+    			sem_wait(&mutex);
+    			sendBase = ack;
+				cwnd += MSS;
+				sem_post(&mutex);
+				dupACKcount = 0;
+				if (cwnd >= ssthresh)
+					mode = CA;
+    		}
+    	} else if (mode == CA) { // Congestion avoidance
+    		if (ack == sendBase) {
+    			dupACKcount++;
+    			if (dupACKcount == 3) {
+    				mode = FR;
+					ssthresh = cwnd / 2;
+    				sem_wait(&mutex);
+					cwnd = MSS;
+					sem_post(&mutex);
+    			}
+    		} else if (ack > sendBase) {
+    			sem_wait(&mutex);
+    			sendBase = ack;
+				cwnd += MSS * MSS / cwnd;
+				sem_post(&mutex);
+				dupACKcount = 0;
+    		}
+    	} else { // Fast recovery
+    		if (ack == sendBase) {
+    			sem_wait(&mutex);
+				cwnd += MSS * MSS / cwnd;
+				sem_post(&mutex);
+    		} else if (ack > sendBase) {
+    			sem_wait(&mutex);
+    			sendBase = ack;
+				cwnd = ssthresh;
+				sem_post(&mutex);
+				dupACKcount = 0;
+    		}
+    	}
+    }
+    return NULL;
+}
+
+void storeFile(char* filename, int packetNum, ull actualBytes) {
+	FILE *fp;
+    fp = fopen(filename, "rb");
+    ull read = 0;
+    for (ull i = 1; i <= packetNum; i++) {
+    	segment* packet = malloc(sizeof(segment));
+    	packet->seqNum = i;
+    	if (i != packetNum) {
+    		packet->length = payload;
+    		read += payload;
+    		packet->end = '0';
+    	} else {
+    		packet->length = actualBytes - read;
+    		packet->end = '1';
+    	}
+    	fread(packet->data, 1, packet->length, fp);
+    	packet->data[packet->length] = '\0';
+    	packetBuffer[i] = packet;
+    }
+}
+
+ull readSize(char* filename, ull bytesToTransfer) {
+	FILE *fp;
+    fp = fopen(filename, "rb");
+    if (!fp)
+        diep("Could not open file to send.");
+    ull fileSize, actualBytes;
+    fseek(fp, 0, SEEK_END);
+    fileSize = ftell(fp);
+    rewind(fp);
+    printf("fileSize = %lld\n", fileSize);
+    if (bytesToTransfer > fileSize) {
+        actualBytes = fileSize;
+        printf("bytesToTransfer too large, use actual bytes %lld.\n", actualBytes);
+    } else
+        actualBytes = bytesToTransfer;
+    fclose(fp);
+    return actualBytes;
+}
+
+void diep(char* s) {
     perror(s);
     exit(1);
 }
 
-typedef struct sharedData {
-    int timeout;
-    int dupAcksCount;
-    int threshold;
-    double CW;
-    int mode;
-    int sendBase;
-    long int timer;
-    int currIdx;
-    bool complete;
-    sem_t sem;
-} shared_data;
-
-#define INIT_sharedData
-
-
-shared_data data;
-
-void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
-    //Open the file
-    FILE *fp;
-    fp = fopen(filename, "rb");
-    if (fp == NULL) {
-        printf("Could not open file to send.");
-        exit(1);
-    }
-
-    /* Determine how many bytes to transfer */
-
-    slen = sizeof (si_other);
-
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-        diep("socket");
-
-    memset((char *) &si_other, 0, sizeof (si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(hostUDPport);
-    if (inet_aton(hostname, &si_other.sin_addr) == 0) {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
-    }
-
-
-    /* Send data and receive acknowledgements on s*/
-    pthread_t t; // ?
-    clock_t start = clock();
-    while(true){
-        if(data.complete){
-            break;
-        }
-
-        sem_wait(&data.sem);
-        int upper = data.sendBase + (int)data.CW;
-        sem_post(&data.sem);
-    }
-    printf("Closing the socket\n");
-    close(s);
-    return;
-
-}
-
-/*
- * 
- */
 int main(int argc, char** argv) {
 
-    unsigned short int udpPort;
-    unsigned long long int numBytes;
+    us udpPort;
+    ull numBytes;
 
     if (argc != 5) {
         fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
         exit(1);
     }
-    udpPort = (unsigned short int) atoi(argv[2]);
+    udpPort = (us) atoi(argv[2]);
     numBytes = atoll(argv[4]);
-
-
 
     reliablyTransfer(argv[1], udpPort, argv[3], numBytes);
 
-
     return (EXIT_SUCCESS);
 }
-
