9efe9bda31986bb6305500139789a027bf029a1f
Yerong <yerong.li@outlook.com>
2019-10-27 20:58:16 -0500

    try

--- mp2/src/sender_main.c
+++ mp2/src/sender_main.c
@@ -82,7 +82,7 @@ void reliablyTransfer(char* hostname, us hostUDPport, char* filename, ull bytesT
         sem_wait(&mutex);
         ull wnEnd = sendBase + cwnd;
         sem_post(&mutex);
-        while (nextSeqNum < packetNum && nextSeqNum < wnEnd) {
+        if (nextSeqNum < packetNum && nextSeqNum < wnEnd) {
             segment* packet = packetBuffer[nextSeqNum];
             sendto(s, packet, sizeof(segment), 0,
                 (struct sockaddr *)&si_other, slen);
